2f9519f1b42cc722995da6996f346727e68de88d
Unknown <daniel.tesanovic97@gmail.com>
2018-03-01 19:52:05 +0000

    History+Forecastv0.02

--- weather-app/src/components/phone/index.js
+++ weather-app/src/components/phone/index.js
@@ -8,22 +8,17 @@ export default class phone extends Component {
 
 	constructor(props){
 		super(props);
-		var x = "London";
-		var y = "20180112"
-		var z = "UK";
-
 		this.state.temp = "";
 		this.state.current = "current";
-
-		this.state.country = "GB";
 		this.state.zmw="00000.40.03779";
 		this.state.date = "";
+		this.state.history="20180220";
 		this.fetchWeatherDataCurrent();
 		this.setState({ display: true });
 	}
 
 	fetchWeatherDataCurrent = () => {
-		var url = "http://api.wunderground.com/api/6e968d28787ddc8c/conditions/q/zmw:"+this.state.zmw+".json";
+		var url = "http://api.wunderground.com/api/03cd34b98761d6d4/conditions/q/zmw:"+this.state.zmw+".json";
 		$.ajax({
 			url: url,
 			dataType: "jsonp",
@@ -34,7 +29,7 @@ export default class phone extends Component {
 	}
 
 	fetchWeatherDataHistory = () => {
-		var url = "http://api.wunderground.com/api/6e968d28787ddc8c/history_"+this.history+"/q/zmw:"+this.state.zmw+".json";
+		var url = "http://api.wunderground.com/api/03cd34b98761d6d4/history_20180220/q/zmw:"+this.state.zmw+".json";
 		$.ajax({
 			url: url,
 			dataType: "jsonp",
@@ -44,7 +39,7 @@ export default class phone extends Component {
 	}
 
 	fetchWeatherDataForecast = () => {
-		var url = "http://api.wunderground.com/api/6e968d28787ddc8c/hourly10day/q/zmw:"+this.state.zmw+".json";
+		var url = "http://api.wunderground.com/api/03cd34b98761d6d4/hourly10day/q/zmw:"+this.state.zmw+".json";
 		$.ajax({
 			url: url,
 			dataType: "jsonp",
@@ -71,66 +66,66 @@ export default class phone extends Component {
 			var datep2 = new Date(date.setTime( date.getTime() + 2 * 86400000 ));
 			var date = new Date();
 			var datep3 = new Date(date.setTime( date.getTime() + 3 * 86400000 ));
+			var month = "FEB";
 
 		return (
 
 				<div class={ style.container }>
-					<div class={ style.header }>
-						<div class={ style.city }>{ this.state.locate }</div>
-						<div class={ style.conditions }>{ this.state.cond }</div>
-						<span class={ tempStyles }>{ this.state.temp }</span>
-					</div>
+				<div class= { style_iphone.container }>
+				{ this.state.display ? <Button number={ "S" }  class={ style_iphone.button } clickFunction={ this.fetchWeatherDataForecast }/ > : null }
+					{ this.state.display ? <Button number={ datem3.getDate() }  class={ style_iphone.button } clickFunction={ this.fetchWeatherDataHistory }/ > : null }
+					{ this.state.display ? <Button number={ datem2.getDate()}  class={ style_iphone.button } clickFunction={ this.fetchWeatherDataHistory }/ > : null }
+					{ this.state.display ? <Button number={ datem1.getDate()}  class={ style_iphone.button } clickFunction={ this.fetchWeatherDataHistory }/ > : null }
+					{ this.state.display ? <Button number={ today.getDate() }  class={ style_iphone.button } clickFunction={ this.fetchWeatherDataCurrent }/ > : null }
+					{ this.state.display ? <Button number={ datep1.getDate() }  class={ style_iphone.button } clickFunction={ this.fetchWeatherDataForecast }/ > : null }
+					{ this.state.display ? <Button number={ datep2.getDate() }  class={ style_iphone.button } clickFunction={ this.fetchWeatherDataForecast }/ > : null }
+					{ this.state.display ? <Button number={ datep3.getDate() }  class={ style_iphone.button } clickFunction={ this.fetchWeatherDataForecast }/ > : null }
+				</div>
+				<div class={ style.month }>{ "FEB" }</div>
+				<div class={ style.city }>{ this.state.locate }</div>
+				<div class={ style.temperature }>{ this.state.temp }</div>
+				<div class={ style.temperature }>{ this.state.cond }</div>
+					
 					<div class={ style.details }></div>
-					<div class= { style_iphone.container }>
-					{ this.state.display ? <Button number={ "S" }  class={ style_iphone.button } clickFunction={ this.fetchWeatherDataForecast }/ > : null }
-						{ this.state.display ? <Button number={ datem3.getDate() }  class={ style_iphone.button } clickFunction={ this.fetchWeatherDataHistory }/ > : null }
-						{ this.state.display ? <Button number={ datem2.getDate()}  class={ style_iphone.button } clickFunction={ this.fetchWeatherDataHistory }/ > : null }
-						{ this.state.display ? <Button number={ datem1.getDate()}  class={ style_iphone.button } clickFunction={ this.fetchWeatherDataHistory }/ > : null }
-						{ this.state.display ? <Button number={ today.getDate() }  class={ style_iphone.button } clickFunction={ this.fetchWeatherDataCurrent }/ > : null }
-						{ this.state.display ? <Button number={ datep1.getDate() }  class={ style_iphone.button } clickFunction={ this.fetchWeatherDataForecast }/ > : null }
-						{ this.state.display ? <Button number={ datep2.getDate() }  class={ style_iphone.button } clickFunction={ this.fetchWeatherDataForecast }/ > : null }
-						{ this.state.display ? <Button number={ datep3.getDate() }  class={ style_iphone.button } clickFunction={ this.fetchWeatherDataForecast }/ > : null }
-					</div>
+					
 				</div>
 			);
-		}/*
-			<div class={ style.container }>
-				<div class={ style.header }>
-					<div class={ style.city }>{ this.state.winddir }</div>
-				</div>
-				<div class={ style.details }></div>
-
-			</div>*/
+		}
 	}
 
 	parseResponseConditions = (parsed_json) => {
-		var location = parsed_json['current_observation']['display_location']['city'];
 		var temp_c = parsed_json['current_observation']['temp_c'];
 		var conditions = parsed_json['current_observation']['weather'];
 
 		// set states for fields so they could be rendered later on
 		this.setState({
-			locate: location,
 			temp: temp_c,
 			cond : conditions
 		});
 	}
 
 	parseResponseHistory = (parsed_json) => {
-		var wdird = parsed_json['history']['observations'][0]['wdird'];
+		var currentTime = new Date();
+		console.log(currentTime.getHours());
+		var tempm = parsed_json['history']['observations'][currentTime.getHours()-1]['tempm'];
+		var conditions = parsed_json['history']['observations'][currentTime.getHours()-1]['conds'];
 
 		// set states for fields so they could be rendered later on
 		this.setState({
-			winddir: wdird
+			cond: conditions,
+			temp : tempm
 		});
 	}
 
 	parseResponseForecast = (parsed_json) => {
-		var wdird = parsed_json['history']['observations'][0]['wdird'];
+		var temp_c = parsed_json['hourly_forecast'][23]['temp']['metric'];
+		var conditions = parsed_json['hourly_forecast'][23]['condition'];
 
 		// set states for fields so they could be rendered later on
 		this.setState({
-			winddir: wdird
+			cond : conditions,
+			temp: temp_c
+			
 		});
 	}
 }
