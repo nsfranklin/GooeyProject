05aeb78718f27e18ddffe55b5079ba6db9468f67
Nick <nickfranklin@rocketmail.com>
2018-03-08 09:52:06 +0000

    fixedMistake

--- weather-app/src/components/windD/index.js
+++ weather-app/src/components/windD/index.js
@@ -15,55 +15,55 @@ chooseIcon() {
 	switch(this.state.currentWindDirection) {
 	
 		case "N":
-			return <img src="/assets/directions/n.svg"/>;
+			return <img src="/assets/directions/n.png"/>;
 			break;
 		case "S":
-			return <img src="/assets/directions/s.svg"/>;		
+			return <img src="/assets/directions/s.png"/>;		
 			break;
 		case "E":
-			return <img src="/assets/directions/e.svg"/>;			
+			return <img src="/assets/directions/e.png"/>;			
 			break;
 		case "W":
-			return <img src="/assets/directions/w.svg"/>;			
+			return <img src="/assets/directions/w.png"/>;			
 			break;
 		case "NE":
-			return <img src="/assets/directions/ne.svg"/>;		
+			return <img src="/assets/directions/ne.png"/>;		
 			break;
 		case "SE":
-			return <img src="/assets/directions/se.svg"/>;	
+			return <img src="/assets/directions/se.png"/>;	
 			break;
 		case "SW":
-			return <img src="/assets/directions/sw.svg"/>;			
+			return <img src="/assets/directions/sw.png"/>;			
 			break;
 		case "NW":
-			return <img src="/assets/directions/nw.svg"/>;			
+			return <img src="/assets/directions/nw.png"/>;			
 			break;
 		case "NNE":
-			return <img src="/assets/directions/nne.svg"/>;			
+			return <img src="/assets/directions/nne.png"/>;			
 			break;
 		case "ENE":	
-			return <img src="/assets/directions/ene.svg"/>;		
+			return <img src="/assets/directions/ene.png"/>;		
 			break;
 		case "ESE":
-			return <img src="/assets/directions/ese.svg"/>;			
+			return <img src="/assets/directions/ese.png"/>;			
 			break;
 		case "SSE":
-			return <img src="/assets/directions/sse.svg"/>;		
+			return <img src="/assets/directions/sse.png"/>;		
 			break;
 		case "SSW":
-			return <img src="/assets/directions/ssw.svg"/>;		
+			return <img src="/assets/directions/ssw.png"/>;		
 			break;
 		case "WSW":
-			return <img src="/assets/directions/wsw.svg"/>;			
+			return <img src="/assets/directions/wsw.png"/>;			
 			break;
 		case "WNW":
-			return <img src="/assets/directions/wnw.svg"/>;			
+			return <img src="/assets/directions/wnw.png"/>;			
 			break;
 		case "NNW":
-			return <img src="/assets/directions/nnw.svg"/>; 			
+			return <img src="/assets/directions/nnw.png"/>; 			
 			break;
 		default:
-			return <img src="/assets/directions/test.svg"/>;	
+			return <img src="/assets/directions/test.png"/>;	
 			break;
 	}
 }
