253a8c30fa1cdc7c3143e729f4c3b9f5fbbff4a3
Nick <nickfranklin@rocketmail.com>
2018-03-09 00:26:32 +0000

    reformated

--- weather-app/src/components/windD/index.js
+++ weather-app/src/components/windD/index.js
@@ -7,55 +7,55 @@ chooseIcon(arg) {
 	switch(arg) {
 
 		case "N":
-			return <img src="/assets/directions/n.png"/>;
+			return <img class='icon' src="/assets/directions/n.png"/>;
 			break;
 		case "S":
-			return <img src="/assets/directions/s.png"/>;
+			return <img class='icon' src="/assets/directions/s.png"/>;
 			break;
 		case "E":
-			return <img src="/assets/directions/e.png"/>;
+			return <img class='icon' src="/assets/directions/e.png"/>;
 			break;
 		case "W":
-			return <img src="/assets/directions/w.png"/>;
+			return <img class='icon' src="/assets/directions/w.png"/>;
 			break;
 		case "NE":
-			return <img src="/assets/directions/ne.png"/>;
+			return <img class='icon' src="/assets/directions/ne.png"/>;
 			break;
 		case "SE":
-			return <img src="/assets/directions/se.png"/>;
+			return <img class='icon' src="/assets/directions/se.png"/>;
 			break;
 		case "SW":
-			return <img src="/assets/directions/sw.png"/>;
+			return <img class='icon' src="/assets/directions/sw.png"/>;
 			break;
 		case "NW":
-			return <img src="/assets/directions/nw.png"/>;
+			return <img class='icon' src="/assets/directions/nw.png"/>;
 			break;
 		case "NNE":
-			return <img src="/assets/directions/nne.png"/>;
+			return <img class='icon' src="/assets/directions/nne.png"/>;
 			break;
 		case "ENE":
-			return <img src="/assets/directions/ene.png"/>;
+			return <img class='icon' src="/assets/directions/ene.png"/>;
 			break;
 		case "ESE":
-			return <img src="/assets/directions/ese.png"/>;
+			return <img class='icon' src="/assets/directions/ese.png"/>;
 			break;
 		case "SSE":
-			return <img src="/assets/directions/sse.png"/>;
+			return <img class='icon' src="/assets/directions/sse.png"/>;
 			break;
 		case "SSW":
-			return <img src="/assets/directions/ssw.png"/>;
+			return <img class='icon' src="/assets/directions/ssw.png"/>;
 			break;
 		case "WSW":
-			return <img src="/assets/directions/wsw.png"/>;
+			return <img class='icon' src="/assets/directions/wsw.png"/>;
 			break;
 		case "WNW":
-			return <img src="/assets/directions/wnw.png"/>;
+			return <img class='icon' src="/assets/directions/wnw.png"/>;
 			break;
 		case "NNW":
-			return <img src="/assets/directions/nnw.png"/>;
+			return <img class='icon' src="/assets/directions/nnw.png"/>;
 			break;
 		default:
-			return <img src="/assets/directions/n.png"/>;
+			return <img class='icon' src="/assets/directions/n.png"/>;
 			break;
 	}
 }
@@ -63,7 +63,6 @@ chooseIcon(arg) {
 mappingT(){
 	var temp = new Array(24);
 	var date = new Date();
-
 	for(var x = 0; x < temp.length; x++)
 	{
 		temp[x] = this.props.dataf['hourly_forecast'][this.props.days*23+x];
@@ -77,7 +76,7 @@ mappingT(){
 				date = new Date();
 				var hours = new Date(date.setTime( date.getTime() + i * 3600000 ));
 				output.push(
-				<div>
+				<div class='windD'>
 					<p> {temp[i]['wspd']['metric']} </p>
 					<p> {temp[i]['wdir']['dir']} </p>
 					<p>{this.chooseIcon(temp[i]['wdir']['dir'])}</p>
